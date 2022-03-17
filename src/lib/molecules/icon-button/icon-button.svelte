bb81d1e56a8593e8eff87564ffd61d01b1737701
Mohab Mohamed <mohab8m@gmail.com>
2022-03-16 21:56:48 -0400

    the initial commit

---
+++ src/lib/molecules/icon-button/icon-button.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Icon from '../../atoms/icon/icon.svelte';
+
+    export let iconData;
+    export let iconScale;
+
+    //optional
+    export let className = undefined;
+</script>
+
+<button class={['p-5 hover:bg-gray-100 rounded-lg', className].join(' ')}>
+    <Icon data={iconData} scale={iconScale} />
+</button>
