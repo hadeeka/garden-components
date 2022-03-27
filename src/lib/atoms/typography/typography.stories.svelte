8ad35b26c8ac114f3477cba9c1a08be8da83502f
Mohab Mohamed <mohab8m@gmail.com>
2022-03-27 17:26:18 -0400

    add typography folder

---
+++ src/lib/atoms/typography/typography.stories.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import Typography from './typography.svelte';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Atoms/Typography" component={Typography} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <Typography {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story name="Default" args={{}} />
