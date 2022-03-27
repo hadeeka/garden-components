138a9de3b04105bf7a3b760877d7e2721fa3d2e7
Mohab Mohamed <mohab8m@gmail.com>
2022-03-27 17:35:09 -0400

    add typography template

---
+++ src/lib/atoms/typography/typography.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let role = 'h1';
+</script>
+
+{#if 'h1' === role}
+    <h1>Headline 1</h1>
+{:else if 'h2' === role}
+    <h2>Headline 2</h2>
+{:else}
+    <h4>Default Case</h4>
+{/if}
