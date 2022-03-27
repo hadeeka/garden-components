d6a6f394c82e62a3d01d824842a6d0a4178188d6
Mohab Mohamed <mohab8m@gmail.com>
2022-03-27 17:20:21 -0400

    edit styles of button

--- src/lib/atoms/button/button.svelte
+++ src/lib/atoms/button/button.svelte
@@ -2,18 +2,7 @@
     export let label = 'default label';
 </script>
 
-<button
-    class={[
-        'px-24',
-        'py-2',
-        'bg-gray-100',
-        'rounded',
-        'border',
-        'border-gray-200',
-        'hover:bg-gray-500',
-        'hover:text-gray-200'
-    ].join(' ')}
->
+<button class={['px-24', 'py-2', 'bg-rose-600', 'text-stone-50', 'rounded', 'hover:shadow-md'].join(' ')}>
     {label}
 </button>
 
