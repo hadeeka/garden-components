027dc2c13e0f582d70eaa74371f0210b75eb15f1
Mohab Mohamed <mohab8m@gmail.com>
2022-03-27 21:43:12 -0400

    decouple typography styles

--- src/lib/atoms/typography/headline/headline.stories.svelte
+++ src/lib/atoms/typography/headline/headline.stories.svelte
@@ -1,15 +1,15 @@
 <script>
     import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
-    import Typography from './typography.svelte';
+    import Headline from './text.svelte';
 </script>
 
 <!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
 <!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
-<Meta title="Atoms/Typography" component={Typography} />
+<Meta title="Atoms/Headline" component={Headline} />
 
 <!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
 <Template let:args>
-    <Typography {...args} />
+    <Headline {...args} />
 </Template>
 
 <!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
