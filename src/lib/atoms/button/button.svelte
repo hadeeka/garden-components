aff68d1cc8a40ec56c9b6d766ca44543123d54ec
Mohab Mohamed <mohab8m@gmail.com>
2022-03-29 21:56:25 -0400

    add button properties

--- src/lib/atoms/button/button.svelte
+++ src/lib/atoms/button/button.svelte
@@ -1,9 +1,34 @@
 <script>
-    export let label = 'default label';
+    import Icon from '../icon/icon.svelte';
+
+    // required properties
+    export let label = '';
+    export let solid = true;
+    export let metamask = false;
+
+    // optional properties
+    export let icon = undefined;
 </script>
 
-<button class={['px-24', 'py-2', 'bg-rose-600', 'text-stone-50', 'rounded', 'hover:shadow-md'].join(' ')}>
-    {label}
+<button
+    class={[
+        'flex',
+        'items-center',
+        'justify-between',
+        'w-72',
+        'px-12',
+        'py-2',
+        solid ? 'bg-rose-600' : 'border border-rose-600',
+        'text-sm',
+        solid ? 'text-stone-50' : 'text-rose-600',
+        'rounded',
+        'hover:shadow-md'
+    ].join(' ')}
+>
+    {#if icon}
+        <Icon data={icon} />
+    {/if}
+    {label.toLocaleUpperCase()}
 </button>
 
 <style>
