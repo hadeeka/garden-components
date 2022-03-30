8c2ab4d8d6b85ba01e831bac8bcd635c127227a1
Mohab Mohamed <mohab8m@gmail.com>
2022-03-30 18:01:34 -0400

    add branding component for buttons

---
+++ src/lib/atoms/button/brands/button.metamask.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Icon from '../../icon/icon.svelte';
+
+    // required properties
+    export let label = '';
+    export let solid = true;
+
+    // optional properties
+    export let icon = undefined;
+    export let metamask = undefined;
+</script>
+
+<button
+    class={[
+        'flex',
+        'items-center',
+        'justify-between',
+        'w-72',
+        'px-12',
+        'py-2',
+        'border border-[#F89C35]',
+        'text-sm',
+        'text-[#F89C35]',
+        'rounded',
+        'hover:shadow-md'
+    ].join(' ')}
+>
+    {#if icon}
+        <Icon data={icon} />
+    {/if}
+    {label.toLocaleUpperCase()}
+</button>
+
+<style>
+</style>
