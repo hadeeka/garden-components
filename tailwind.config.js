52fcf9b6be3e99817ea746ee14b2f557d2da6d62
Mohab Mohamed <mohab8m@gmail.com>
2022-03-29 21:58:21 -0400

    add metamask color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     content: ['./src/**/*.{html,svelte,js}'],
     theme: {
+        colors: {
+            metamask: '#F89C35'
+        },
         extend: {}
     },
     plugins: []
