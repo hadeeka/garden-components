d2dce0249f2b6a16fca7ee7418ed604f72b0c19a
Mohab Mohamed <mohab8m@gmail.com>
2022-03-31 22:36:32 -0400

    add initial tabs component

---
+++ src/lib/organisms/tabs/tabs.stories.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import Tabs from './tabs.svelte';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Organisms/Tabs" component={Tabs} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <Tabs {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story name="Default" args={{}} />
---
+++ src/lib/organisms/tabs/tabs.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Icon from '../../atoms/icon/icon.svelte';
+    import {default as house} from '../../../assets/icons/house';
+</script>
+
+<ul class={['relative', 'flex', 'justify-between', 'items-center', 'w-full', 'h-16', 'shadow-md'].join(' ')}>
+    <div class={['absolute', 'left-0/4', 'top-0', 'w-1/4', 'h-1', 'bg-rose-600'].join(' ')} />
+    <li class={['flex', 'w-1/4', 'justify-center', 'items-center'].join(' ')}>
+        <a href="#a"><Icon class={['text-rose-600'].join(' ')} data={house} /></a>
+    </li>
+    <li class={['flex', 'w-1/4', 'justify-center', 'items-center'].join(' ')}>
+        <a href="#a"><Icon class={['text-rose-600'].join(' ')} data={house} /></a>
+    </li>
+    <li class={['flex', 'w-1/4', 'justify-center', 'items-center'].join(' ')}>
+        <a href="#a"><Icon class={['text-rose-600'].join(' ')} data={house} /></a>
+    </li>
+    <li class={['flex', 'w-1/4', 'justify-center', 'items-center'].join(' ')}>
+        <a href="#a"><Icon class={['text-rose-600'].join(' ')} data={house} /></a>
+    </li>
+</ul>
