caeeffafec1923883119686dc00b1ea06c8bb2ca
Mohab Mohamed <mohab8m@gmail.com>
2022-03-31 23:10:59 -0400

    add tabs slider animation

--- src/lib/organisms/tabs/tabs.svelte
+++ src/lib/organisms/tabs/tabs.svelte
@@ -1,20 +1,48 @@
 <script>
     import Icon from '../../atoms/icon/icon.svelte';
     import {default as house} from '../../../assets/icons/house';
+    import {default as clock} from '../../../assets/icons/clock';
+    import {default as star} from '../../../assets/icons/star';
+    import {default as trash} from '../../../assets/icons/trash';
+
+    let sliderPos = 0;
+
+    const handleSlide = ev => {
+        sliderPos = ev.target.dataset['p'];
+    };
 </script>
 
 <ul class={['relative', 'flex', 'justify-between', 'items-center', 'w-full', 'h-16', 'shadow-md'].join(' ')}>
-    <div class={['absolute', 'left-0/4', 'top-0', 'w-1/4', 'h-1', 'bg-rose-600'].join(' ')} />
-    <li class={['flex', 'w-1/4', 'justify-center', 'items-center'].join(' ')}>
-        <a href="#a"><Icon class={['text-rose-600'].join(' ')} data={house} /></a>
+    <div
+        class={[
+            'inline-block',
+            'absolute',
+            sliderPos > 0 ? `left-${sliderPos}/4` : 'left-0',
+            'top-0',
+            'w-1/4',
+            'h-1',
+            'bg-rose-600',
+            'transition-all'
+        ].join(' ')}
+    />
+    <li class={['flex', 'w-1/4', 'h-full', 'justify-center', 'items-center'].join(' ')} on:click={handleSlide}>
+        <a href="#a" class={['w-full', 'h-full', 'flex', 'justify-center', 'items-center'].join(' ')} data-p="0"
+            ><Icon class={['text-rose-600'].join(' ')} data={house} /></a
+        >
     </li>
-    <li class={['flex', 'w-1/4', 'justify-center', 'items-center'].join(' ')}>
-        <a href="#a"><Icon class={['text-rose-600'].join(' ')} data={house} /></a>
+    <li class={['flex', 'w-1/4', 'h-full', 'justify-center', 'items-center'].join(' ')} on:click={handleSlide}>
+        <a href="#a" class={['w-full', 'h-full', 'flex', 'justify-center', 'items-center'].join(' ')} data-p="1"
+            ><Icon class={['text-rose-600'].join(' ')} data={clock} /></a
+        >
     </li>
-    <li class={['flex', 'w-1/4', 'justify-center', 'items-center'].join(' ')}>
-        <a href="#a"><Icon class={['text-rose-600'].join(' ')} data={house} /></a>
+    <li class={['flex', 'w-1/4', 'h-full', 'justify-center', 'items-center'].join(' ')} on:click={handleSlide}>
+        <a href="#a" class={['w-full', 'h-full', 'flex', 'justify-center', 'items-center'].join(' ')} data-p="2"
+            ><Icon class={['text-rose-600'].join(' ')} data={star} /></a
+        >
     </li>
-    <li class={['flex', 'w-1/4', 'justify-center', 'items-center'].join(' ')}>
-        <a href="#a"><Icon class={['text-rose-600'].join(' ')} data={house} /></a>
+    <li class={['flex', 'w-1/4', 'h-full', 'justify-center', 'items-center'].join(' ')} on:click={handleSlide}>
+        <a href="#a" class={['w-full', 'h-full', 'flex', 'justify-center', 'items-center'].join(' ')} data-p="3"
+            ><Icon class={['text-rose-600'].join(' ')} data={trash} /></a
+        >
     </li>
 </ul>
