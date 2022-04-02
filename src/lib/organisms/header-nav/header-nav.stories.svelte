fea339b6eaa7a03914ef28cffbd112f79313210b
Mohab Mohamed <mohab8m@gmail.com>
2022-04-01 21:30:23 -0400

    add default story

---
+++ src/lib/organisms/header-nav/header-nav.stories.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import HeaderNav from './header-nav.svelte';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Organisms/Header Navigation" component={HeaderNav} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <HeaderNav {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story name="Default" args={{}} />
