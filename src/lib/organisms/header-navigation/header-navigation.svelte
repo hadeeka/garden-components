7297f3d334a846543c1a2637e5a4f8fd04330a81
Mohab Mohamed <mohab8m@gmail.com>
2022-04-06 21:02:10 -0400

    import components into header naviagtion

---
+++ src/lib/organisms/header-navigation/header-navigation.svelte
@@ -0,0 +1,10 @@
+<script>
+    import IconButton from '../../molecules/icon-button';
+    import SearchField from '../search-field';
+</script>
+
+<div>
+    <IconButton />
+    <SearchField />
+    <IconButton />
+</div>
