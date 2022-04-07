da53a5cd27a4f290f21124daa8c4bc3f9cb44f73
Mohab Mohamed <mohab8m@gmail.com>
2022-04-06 21:33:58 -0400

    update stylings of header navigation

--- src/lib/organisms/header-navigation/header-navigation.svelte
+++ src/lib/organisms/header-navigation/header-navigation.svelte
@@ -1,11 +1,14 @@
 <script>
     import IconButton from '../../molecules/icon-button/icon-button.svelte';
     import Variant from '../../../enumerations/variant';
-    import SearchField from '../search-field/search-field.svelte';
+    import SearchField from '../../molecules/search-field/search-field.svelte';
+
+    import {default as bars} from '../../../assets/icons/bars';
+    import {default as user} from '../../../assets/icons/user';
 </script>
 
-<div class={['flex', 'justify-evenly', 'items-center', 'w-full', 'h-16', 'shadow-md'].join(' ')}>
-    <IconButton variant={Variant.TEXT} />
-    <SearchField />
-    <IconButton variant={Variant.TEXT} />
+<div class={['flex', 'justify-between', 'items-center', 'w-full', 'h-16', 'shadow-md', 'p-3', 'bg-stone-50'].join(' ')}>
+    <IconButton variant={Variant.TEXT} data={bars} scale={1.25} />
+    <div class={['w-2/3'].join(' ')}><SearchField /></div>
+    <IconButton variant={Variant.TEXT} data={user} scale={1.25} />
 </div>
