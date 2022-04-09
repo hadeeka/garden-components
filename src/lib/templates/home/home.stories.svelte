473cae3d70c8d7917090f14edcb0c795d9b4635b
Mohab Mohamed <mohab8m@gmail.com>
2022-04-08 21:26:59 -0400

    create home folder template

---
+++ src/lib/templates/home/home.stories.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import Home from './home.svelte';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Templates/Home" component={Home} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <Home {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story name="Default" args={{}} />
