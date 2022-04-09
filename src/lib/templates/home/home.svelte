e52ec8dfa08ff8b7e4900efd37eabd123a72af27
Mohab Mohamed <mohab8m@gmail.com>
2022-04-08 23:14:17 -0400

    build home template layout

--- src/lib/templates/home/home.svelte
+++ src/lib/templates/home/home.svelte
@@ -2,10 +2,19 @@
     import HeaderNavigation from '../../organisms/header-navigation/header-navigation.svelte';
     import Clusters from '../../organisms/clusters/clusters.svelte';
     import Tabs from '../../organisms/tabs/tabs.svelte';
+
+    let clustersWidth;
+    let clustersHeight;
 </script>
 
-<div>
-    header
-    <div>cluster container</div>
-    <div>tabs</div>
+<div class={['h-full', 'w-full', 'flex', 'flex-col', 'justify-between', 'items-center'].join(' ')}>
+    <HeaderNavigation />
+    <div
+        bind:clientWidth={clustersWidth}
+        bind:clientHeight={clustersHeight}
+        class={['h-full', 'w-full', 'flex', 'justify-center', 'items-center'].join(' ')}
+    >
+        <Clusters width={clustersWidth} height={clustersHeight} />
+    </div>
+    <Tabs />
 </div>
