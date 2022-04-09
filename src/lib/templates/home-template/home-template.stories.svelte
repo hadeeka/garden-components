c138f4aafebc537b45fa80666a0ed3711a9521c7
Mohab Mohamed <mohab8m@gmail.com>
2022-04-08 21:27:25 -0400

    rename old component

--- src/lib/templates/home-template/home-template.stories.svelte
+++ src/lib/templates/home-template/home-template.stories.svelte
@@ -5,7 +5,7 @@
 
 <!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
 <!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
-<Meta title="Templates/Home" component={HomeTemplate} />
+<Meta title="Templates/Home POC" component={HomeTemplate} />
 
 <!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
 <Template let:args>
