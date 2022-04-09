83f850a7d2d08caa7ce87928c0430aa58fef6edb
Mohab Mohamed <mohab8m@gmail.com>
2022-04-08 21:31:01 -0400

    wireframe home tempalte

--- src/lib/templates/home/home.svelte
+++ src/lib/templates/home/home.svelte
@@ -1,2 +1,11 @@
 <script>
+    import HeaderNavigation from '../../organisms/header-navigation/header-navigation.svelte';
+    import Clusters from '../../organisms/clusters/clusters.svelte';
+    import Tabs from '../../organisms/tabs/tabs.svelte';
 </script>
+
+<div>
+    header
+    <div>cluster container</div>
+    <div>tabs</div>
+</div>
