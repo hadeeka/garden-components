5ce90cc66e13ad56113b3c3e042238b68097b328
Mohab Mohamed <mohab8m@gmail.com>
2022-04-10 21:10:46 -0400

    add basic omponents and stylings

--- src/lib/organisms/side-menu/side-menu.svelte
+++ src/lib/organisms/side-menu/side-menu.svelte
@@ -1,2 +1,6 @@
 <script>
+    import Overlay from '../../atoms/overlay/overlay.svelte';
 </script>
+
+<Overlay />
+<div class={['absolute', 'inset-0', 'bg-stone-50', 'w-5/6', 'max-w-xs', 'z-20'].join(' ')} />
