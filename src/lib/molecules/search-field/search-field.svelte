6969ae575bd2ceb807a091ae44eb3b62eaf1a17a
Mohab Mohamed <mohab8m@gmail.com>
2022-04-11 22:23:35 -0400

    add support for search header in mobile view

--- src/lib/molecules/search-field/search-field.svelte
+++ src/lib/molecules/search-field/search-field.svelte
@@ -1,12 +1,32 @@
 <script>
     import Icon from '../../atoms/icon/icon.svelte';
+    import IconButton from '../icon-button/icon-button.svelte';
     import {default as search} from '../../../assets/icons/search';
+    import {default as circleXMark} from '../../../assets/icons/circle-xmark';
+    import {createEventDispatcher} from 'svelte';
+
+    let inputFocus = false;
+
+    const dispatch = createEventDispatcher();
+
+    const handleSearchFocus = () => {
+        dispatch('searchFieldFocus');
+        inputFocus = true;
+    };
+
+    const handleSearchBlur = () => {
+        inputFocus = false;
+        dispatch('searchFieldBlur');
+    };
 </script>
 
 <label class="relative block">
     <span class="sr-only">Search</span>
-    <span class="absolute h-4 left-3 top-0 right-0 bottom-0 m-auto flex">
-        <Icon class={['text-stone-600'].join(' ')} data={search} scale={1} />
+    <span
+        class="absolute h-4 left-3 top-0 right-3 bottom-0 m-auto flex justify-between pointer-events-none text-stone-600"
+    >
+        <Icon data={search} scale={1} />
+        {#if inputFocus} <Icon data={circleXMark} scale={1} /> {/if}
     </span>
     <input
         class={[
@@ -15,7 +35,7 @@
             'bg-stone-200',
             'w-full',
             'rounded-md',
-            'py-1.5',
+            'py-1',
             'pl-9',
             'pr-3',
             'focus:outline-none',
@@ -24,6 +44,8 @@
             'focus:ring-1',
             'sm:text-sm'
         ].join(' ')}
+        on:focus={handleSearchFocus}
+        on:blur={handleSearchBlur}
         placeholder="Search for anything..."
         type="text"
         name="search"
