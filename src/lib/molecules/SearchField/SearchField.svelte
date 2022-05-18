f51c1da50792f2fdcb4c9a12db8f99acd8921088
Mohab Mohamed <mohab8m@gmail.com>
2022-05-17 21:14:51 -0400

    standardize file naming convention

---
+++ src/lib/molecules/SearchField/SearchField.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Icon from '../../atoms/icon/icon.svelte';
+    import IconButton from '../IconButton/IconButton';
+    import {default as search} from '../../../assets/icons/search';
+    import {default as circleXMark} from '../../../assets/icons/circle-xmark';
+    import {createEventDispatcher} from 'svelte';
+
+    let inputFocus = false;
+
+    const dispatch = createEventDispatcher();
+
+    const handleSearchFocus = () => {
+        dispatch('searchFieldFocus');
+        inputFocus = true;
+    };
+
+    const handleSearchBlur = () => {
+        inputFocus = false;
+        dispatch('searchFieldBlur');
+    };
+</script>
+
+<label class="relative block">
+    <span class="sr-only">Search</span>
+    <span
+        class="absolute h-4 left-3 top-0 right-3 bottom-0 m-auto flex justify-between pointer-events-none text-stone-600"
+    >
+        <Icon data={search} scale={1} />
+        {#if inputFocus} <Icon data={circleXMark} scale={1} /> {/if}
+    </span>
+    <input
+        class="placeholder:text-stone-600 block bg-stone-200 w-full rounded-md py-1 pl-9 pr-8 focus:outline-none focus:border-gray-400 focus:ring-gray-400 focus:ring-1 sm:text-sm"
+        on:focus={handleSearchFocus}
+        on:blur={handleSearchBlur}
+        placeholder="Search for anything..."
+        type="text"
+        name="search"
+    />
+</label>
