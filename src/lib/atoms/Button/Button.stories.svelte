9fca5c3dd211ffdc0d40c4ce3700fbc52539b83d
Mohab Mohamed <mohab8m@gmail.com>
2022-05-18 22:25:59 -0400

    standardize file naming conventions

---
+++ src/lib/atoms/Button/Button.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import Button from './button.svelte';
+    import {default as fox} from '../../../assets/icons/metamask/fox';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Atoms/Button" component={Button} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <Button {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story name="Default" args={{}} />
+<Story name="Solid w/ Label" args={{label: 'Solid Button'}} />
+<Story name="Outline w/ Label" args={{label: 'Outline Button', solid: false}} />
---
+++ src/lib/atoms/Button/brands/button.metamask.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import MetamaskButton from './button.metamask.svelte';
+    import {default as fox} from '../../../../assets/icons/metamask/fox';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Atoms/Metamask Button" component={MetamaskButton} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <MetamaskButton {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story name="Default" args={{}} />
+<Story name="Connect to Metamask" args={{label: 'Connect to Metamask', solid: false, metamask: true}} />
