76527391762201c6334981eddc7eca1e0407d7d3
Mohab Mohamed <mohab8m@gmail.com>
2022-05-24 23:11:33 -0400

    standardize folder naming

---
+++ src/lib/Atoms/Overlay/Overlay.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import Overlay from './Overlay.svelte';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Atoms/Overlay" component={Overlay} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <p>EXAMPLE TEXT FROM STORYBOOK</p>
+    <Overlay {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story name="Default" args={{}} />
---
+++ src/lib/Atoms/Overlay/Overlay.svelte
@@ -0,0 +1,4 @@
+<div
+    on:click
+    class={['absolute', 'inset-0', 'h-full', 'w-full', 'backdrop-blur-sm', 'bg-stone-300/30', 'z-10'].join(' ')}
+/>
