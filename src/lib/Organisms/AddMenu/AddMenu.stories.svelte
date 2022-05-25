38fb8c5a3b58c739e88ca05153651987940e17da
Mohab Mohamed <mohab8m@gmail.com>
2022-05-24 23:15:49 -0400

    standardize folder naming

---
+++ src/lib/Organisms/AddMenu/AddMenu.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import AddMenu from './AddMenu.svelte';
+    import Overlay from '../../atoms/Overlay/Overlay.svelte';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Organisms/Add Menu" component={AddMenu} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <Overlay />
+    <AddMenu {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story name="Default" args={{}} />
---
+++ src/lib/Organisms/Tabs/Tabs.svelte
@@ -0,0 +1,48 @@
+<script>
+    import Icon from '../../atoms/icon/icon.svelte';
+    import {default as house} from '../../../assets/icons/house';
+    import {default as clock} from '../../../assets/icons/clock';
+    import {default as star} from '../../../assets/icons/star';
+    import {default as trash} from '../../../assets/icons/trash';
+
+    let sliderPos = 0;
+
+    const handleSlide = ev => {
+        sliderPos = ev.target.dataset['p'];
+    };
+</script>
+
+<ul class="relative flex flex-none justify-between items-center w-full h-16 shadow-md">
+    <div
+        class={[
+            'inline-block',
+            'absolute',
+            sliderPos > 0 ? `left-${sliderPos}/4` : 'left-0',
+            'top-0',
+            'w-1/4',
+            'h-1',
+            'bg-rose-600',
+            'transition-all'
+        ].join(' ')}
+    />
+    <li class="flex w-1/4 h-full justify-center items-center" on:click={handleSlide}>
+        <a href="#a" class="w-full h-full flex justify-center items-center" data-p="0"
+            ><Icon class="text-rose-600" data={house} /></a
+        >
+    </li>
+    <li class="flex w-1/4 h-full justify-center items-center" on:click={handleSlide}>
+        <a href="#a" class="w-full h-full flex justify-center items-center" data-p="1"
+            ><Icon class="text-rose-600" data={clock} /></a
+        >
+    </li>
+    <li class="flex w-1/4 h-full justify-center items-center" on:click={handleSlide}>
+        <a href="#a" class="w-full h-full flex justify-center items-center" data-p="2"
+            ><Icon class="text-rose-600" data={star} /></a
+        >
+    </li>
+    <li class="flex w-1/4 h-full justify-center items-center" on:click={handleSlide}>
+        <a href="#a" class="w-full h-full flex justify-center items-center" data-p="3"
+            ><Icon class="text-rose-600" data={trash} /></a
+        >
+    </li>
+</ul>
